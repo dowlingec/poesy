8460c5b1f44c0b8d8c81a5d11fb3c4e875541318
Elizabeth Dowling <dowlinge94@gmail.com>
2022-04-14 14:48:59 -0400

    backend updates on submit

--- src/components/NewPoemForm.js
+++ src/components/NewPoemForm.js
@@ -4,12 +4,28 @@ function NewPoemForm() {
   const [title, setTitle] = useState("")
   const [author, setAuthor] = useState("")
   const [content, setContent] = useState("")
+  
 
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault()
     alert("SUBMIT INFO")
+    const newPoem = {
+      "title": title,
+      "author": author,
+      "content": content,
+    };
+    
     (async () => {
-      
-    })
+      let req = await fetch(`http://localhost:8004/poems`,
+      {
+        method: "POST",
+        headers: {"Content-Type" : "application/json"},
+        body : JSON.stringify(newPoem)
+      }
+      )
+      let newObj = req.json()
+      console.log("new one", newObj)
+    })()
   }
 
   return (
@@ -18,7 +34,7 @@ function NewPoemForm() {
       <input placeholder="Author" onChange={(event) => {setAuthor(event.target.value)}}/>
       <textarea placeholder="Write your masterpiece here..." rows={10} onChange={(event) => {setContent(event.target.value)}}/>
       <input type="submit" value="Share your masterpiece" />
-      {console.log("user input", title, author, content)}
+      {/* {console.log("user input", title, author, content)} */}
     </form>
   );
 }
