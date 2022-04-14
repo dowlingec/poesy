aea9af11b7f5f8ec6649bc1e394ff1a853a05dfb
Elizabeth Dowling <dowlinge94@gmail.com>
2022-04-14 14:08:12 -0400

    deliverable 1 complete

--- src/components/Poem.js
+++ src/components/Poem.js
@@ -1,12 +1,14 @@
 import React from "react";
 
-function Poem() {
+function Poem({poem}) {
+  const {title, content, author } = poem
+  
   return (
     <div>
-      <h3>Title</h3>
-      <p>Content</p>
+      <h3>{title}</h3>
+      <p>{content}</p>
       <p>
-        <strong>- By Author</strong>
+        <strong>- By {author}</strong>
       </p>
       <button>Mark as read</button>
     </div>
--- src/components/PoemsContainer.js
+++ src/components/PoemsContainer.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, {useState, useEffect } from "react";
 import Poem from "./Poem";
 
 function PoemsContainer() {
+  const [poemsData, setPoemsData] = useState([])
+
+  useEffect(() =>{
+    (async () => {
+      let req = await fetch(`http://localhost:8004/poems`)
+      let res = await req.json()
+      // console.log("FETCHED DB", res)
+      setPoemsData(res)
+    })()
+  }, [])
+  console.log("fetch data", poemsData)
   return (
     <div className="poems-container">
-      {/* render a list of <Poem> components in here */}
+      { poemsData.map((poem) => {
+        return <Poem key={poem.id} poem={poem}/>
+      })}
     </div>
   );
 }
