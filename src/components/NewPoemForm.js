a9677e9cd785475f9f80dbcea9da48076070aac1
Elizabeth Dowling <dowlinge94@gmail.com>
2022-04-14 14:31:36 -0400

    form value grab working

--- src/components/NewPoemForm.js
+++ src/components/NewPoemForm.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 function NewPoemForm() {
+  const [title, setTitle] = useState("")
+  const [author, setAuthor] = useState("")
+  const [content, setContent] = useState("")
+
+  const handleSubmit = () => {
+    alert("SUBMIT INFO")
+    (async () => {
+      
+    })
+  }
+
   return (
-    <form className="new-poem-form">
-      <input placeholder="Title" />
-      <input placeholder="Author" />
-      <textarea placeholder="Write your masterpiece here..." rows={10} />
+    <form className="new-poem-form" onSubmit={handleSubmit}>
+      <input placeholder="Title" onChange={(event) => {setTitle(event.target.value)}}/>
+      <input placeholder="Author" onChange={(event) => {setAuthor(event.target.value)}}/>
+      <textarea placeholder="Write your masterpiece here..." rows={10} onChange={(event) => {setContent(event.target.value)}}/>
       <input type="submit" value="Share your masterpiece" />
+      {console.log("user input", title, author, content)}
     </form>
   );
 }
